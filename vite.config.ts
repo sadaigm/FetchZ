3abc5eb53c7d402842cc145a800f3647acdf45ae
sadaigm <sadaigm@gmail.com>
2025-07-26 17:51:02 -0400

    update base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/FetchZ/",
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       manifest: {
-        name: 'FetchZ',
-        short_name: 'FetchZ',
-        description: 'A user-friendly tool for managing API requests locally',
-        theme_color: '#ffffff',
+        name: "FetchZ",
+        short_name: "FetchZ",
+        description: "A user-friendly tool for managing API requests locally",
+        theme_color: "#ffffff",
         icons: [
           {
-            src: '/fetchz_512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: "/fetchz_512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
         ],
       },
       devOptions: {
         enabled: true,
-      }
+      },
     }),
   ],
-})
+});
